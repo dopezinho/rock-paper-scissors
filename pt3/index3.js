b426440c001483b06bf7ae5137621913707538dc
dopezinho <opugnero@gmail.com>
2023-04-26 12:04:01 -0300

    pt3 done

--- pt3/index3.js
+++ pt3/index3.js
@@ -1,4 +1,4 @@
-const score = {
+let score = JSON.parse(localStorage.getItem("score")) || {
   wins: 0,
   losses: 0,
   ties: 0,
@@ -42,6 +42,8 @@ function playGame(playerMove) {
   } else if (result === "tie.") {
     score.ties += 1;
   }
+
+  localStorage.setItem("score", JSON.stringify(score));
 
   alert(
     `you picked ${playerMove} and the computer picked ${computerMove}, so ${result}
