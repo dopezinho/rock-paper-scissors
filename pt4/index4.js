344ed21d714b73152b0d585445f8bd0a7594e597
dopezinho <opugnero@gmail.com>
2023-04-27 15:37:22 -0300

    domzin

--- pt4/index4.js
+++ pt4/index4.js
@@ -1,6 +1,1 @@
-document.title = "D.O.M.";
-
-document.body.innerHTML =
-  "<button>aloooo galera de cowboy</button><h2>alo glr de peaum</h2>";
-
-console.log(document.body.innerHTML);
+document.title = "D.O.Miniqueniquenique";
